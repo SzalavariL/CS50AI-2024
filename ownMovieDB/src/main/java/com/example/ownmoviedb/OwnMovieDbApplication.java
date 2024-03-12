4a95309613b2d47eab6ad91ae964ccddb2310646
szalavari_lajos <szlvrljs@protonmail.com>
2024-03-12 16:32:52 +0100

    ownMovieDB project start, packages system

---
+++ ownMovieDB/src/main/java/com/example/ownmoviedb/OwnMovieDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ownmoviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnMovieDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnMovieDbApplication.class, args);
+    }
+
+}
---
+++ ownMovieDB/src/test/java/com/example/ownmoviedb/OwnMovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ownmoviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnMovieDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
